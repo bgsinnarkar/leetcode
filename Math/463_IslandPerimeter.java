02ac91d61c086c9bfc857858a9fe3500f3dd4f73
Bhupesh Sinnarkar <SINNARBG@legal.regn.net>
2020-07-07 21:07:32 -0400

    Few more problems

---
+++ Math/463_IslandPerimeter.java
@@ -0,0 +1,36 @@
+/***********************************************
+
+Time complexity : O(n * m) whichever is greater
+Space complexity : O(n * m)
+
+https://leetcode.com/problems/island-perimeter/
+
+***********************************************/
+
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        
+        if(grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        
+        int perimeter = 0;
+        
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[i].length; j++) {
+                if(grid[i][j] == 1) {
+                    perimeter += 4;
+                    
+                    if(i > 0 && grid[i-1][j] == 1) {
+                        perimeter -= 2;
+                    }
+                    if(j > 0 && grid[i][j-1] == 1) {
+                        perimeter -= 2;
+                    }                    
+                }
+            }
+        }
+        
+        return perimeter;
+    }
+}
